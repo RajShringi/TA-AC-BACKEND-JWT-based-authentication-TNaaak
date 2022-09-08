c070cd1a897dcd4146064b3368ede16f6e92f00f
raj shringi <raj24shringi@gmail.com>
2022-09-08 20:33:10 +0530

    return articles in article response format

--- block-BNaaen/conduit/models/Article.js
+++ block-BNaaen/conduit/models/Article.js
@@ -9,7 +9,6 @@ const articleSchema = new Schema(
     description: { type: String, required: true },
     body: { type: String, required: true },
     tagList: [{ type: String }],
-    favoritesCount: { type: Number, default: 0 },
     author: { type: Schema.Types.ObjectId, ref: "User", required: true },
     comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
     favoritedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
@@ -19,9 +18,24 @@ const articleSchema = new Schema(
 
 articleSchema.pre("save", function (next) {
   if (this.title && this.isModified("title")) {
-    this.slug = `${slugify(this.title)}-${Date.now()}`;
+    this.slug = `${slugify(this.title.toLowerCase())}-${Date.now()}`;
   }
   next();
 });
+
+articleSchema.methods.articleJSON = function (user = null) {
+  return {
+    slug: this.slug,
+    title: this.title,
+    description: this.description,
+    body: this.body,
+    tagList: this.tagList,
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt,
+    favorited: user ? this.favoritedBy.includes(user.id) : false,
+    favoritesCount: this.favoritedBy.length,
+    author: this.author.profileJSON(user),
+  };
+};
 
 module.exports = mongoose.model("Article", articleSchema);
